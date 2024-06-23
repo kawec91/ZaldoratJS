38b9ffbb1fd2253824e0b67168f62695e408e87a
Lukasz Lukawczyk Notebook Parabola Asus <lukasz.lukawczyk@gmail.com>
2024-06-23 20:40:14 +0200

    Login functionality

--- frontend/src/components/Page/LoginPage/LoginPage.jsx
+++ frontend/src/components/Page/LoginPage/LoginPage.jsx
@@ -1,37 +1,61 @@
 import React, { useState } from 'react';
 import { defaultInput } from '../../Styles/style.js';
+import { useMutation } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
 
 export default function LoginPage() {
-    const [newUser, setNewUser] = useState({
+    const [user, setUser] = useState({
         username: '',
-        email: '',
-        nick: '',
-        name: '',
         password: '',
-        password2: '',
     });
     const handleChanges = (e) => {
-        setNewUser({
-            ...newUser,
+        setUser({
+            ...user,
             [e.target.name]: e.target.value,
         })
-        console.log(newUser);
+        console.log(user);
     }
+
+    const {
+        mutate:loginMutation,isPending, isError, error} = useMutation({
+        mutationFn: async ({username, password}) => {
+            try {
+                const res = await fetch('/api/auth/login', {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({username,password})
+                });
+
+                const data = await res.json();
+                if(!res.ok) throw new Error(data.error || "Faild to login");
+                console.log(data);
+                return data;
+            } catch (error) {
+                throw new Error(error);
+            }
+        },
+        onSuccess: (data) => {
+            toast.success("Login succesful.")
+        }
+    });
 
     const sendData = (e) => {
         e.preventDefault();
-        console.log('Send');
+        loginMutation(user);
     }
 
-    const isError = false;
   return (
     <div className='h-screen w-full flex items-center justify-center'>
         <div className='border-[1px] border-black p-6 w-1/4'>
             <form className='flex flex-col items-center justify-center gap-4' onSubmit={(e) => {sendData(e)}}>
                 <input placeholder='Login' type='text' name='username' onChange={(e) => handleChanges(e)} className={defaultInput}/>
                 <input placeholder='Hasło' type='password' name='password' onChange={(e) => handleChanges(e)} className={defaultInput}/>
-                <button type='submit'>Zaloguj</button>
-                {!isError ? <></> : <p className='text-red-600'>Something went wrong.</p>}
+                <button type='submit'>
+                    {isPending ? "Loading..." : "Login"}
+                </button>
+                {!isError ? <></> : <p className='text-red-600'>{error.message}</p>}
             </form>
         </div>
     </div>
