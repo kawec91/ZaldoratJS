c587709706a1b712b46ffc7deeef9b999e3779c2
Lukasz Lukawczyk Notebook Parabola Asus <lukasz.lukawczyk@gmail.com>
2024-06-28 18:26:46 +0200

    Prepare edit functionality, added delete functionality in RaceAdmin

--- frontend/src/components/AdminRaceTableRow/AdminRaceTableRow.jsx
+++ frontend/src/components/AdminRaceTableRow/AdminRaceTableRow.jsx
@@ -1,14 +1,60 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import React from 'react'
+import toast from 'react-hot-toast';
 
 export default function AdminRaceTableRow({element}) {
-    const openInputFields = () => {
-        const inputs = document.getElementsByTagName('input');
-        for(let i of inputs) {
-            i.disabled = false
+    const queryClient = useQueryClient();
+
+    const openInputFields = (id) => {
+        const trElement = document.getElementById(id)
+        const inputs = trElement.getElementsByTagName('input');
+        
+            for(let i of inputs) {
+                if(i.disabled){
+                    i.disabled = false;
+                } else {
+                    i.disabled = true;
+                    //TODO: Save in DB
+                    
+                    //Invalidate query - refetch raceList
+                    queryClient.invalidateQueries({queryKey: ["racesList"]})
+                }
+            }
+    }
+
+    const {mutate: deleteRace, isPending, isSuccess, isError, error} = useMutation({
+        mutationFn: async(id) => {
+            try {
+                const res = await fetch(`/api/races/delete/${id}`,{
+                    method: "DELETE",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({id})
+                })
+                
+                
+                const data = await res.json();
+                if(!res.ok) throw new Error(data.error || "Faild to create account");
+                console.log(data);
+                return data;
+            } catch (error) {
+                console.log(error);
+                throw error;
+            }
+        },
+        onSuccess: (data) => {
+            toast.success(data.message)
+            //Invalidate query - refetch raceList
+            queryClient.invalidateQueries({queryKey: ["racesList"]})
+        },
+        onError: (data) => {
+            toast.error(data.message);
         }
-      }
+    });
+
   return (
-    <tr key={element.name}>
+    <tr key={element.name} id={element._id}>
         <td>
             <input value={element.name} disabled />
         </td>
@@ -27,8 +73,9 @@ export default function AdminRaceTableRow({element}) {
         <td>
             <input value={element.attributes.intelligence} disabled />
         </td>
-        <td>
-            <button onClick={() => {openInputFields()}}>Edit</button>
+        <td className='flex items-center gap-2'>
+            <button onClick={() => {openInputFields(element._id)}}>Edit</button>
+            <button onClick={()=>{deleteRace(element._id)}} className='text-red-600'>Delete</button>
         </td>
     </tr>
   )
